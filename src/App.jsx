74776e1a6da223c35873fe166f5dc9e8f84196a4
Maria Naveen <marianaveen15@outlook.com>
2024-08-22 18:12:51 +0530

    Dynamically rendered the component using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import BookReview from "./components/BookReview";
 const App = () => {
   return (
-    <div className="w-full">
+    <div className="w-full flex flex-col gap-4">
       <header className="w-full text-lg bg-red-400 p-4 text-center">
         The Book Review Task
       </header>
-      <BookReview></BookReview>
+      <BookReview
+        title="How to win friends?"
+        author="Dale Carnegie"
+        review="Really good!"
+        rating="4 stars"
+      ></BookReview>
+      <BookReview
+        title="Atomic Habits"
+        author="James Clear"
+        review="Fantastic!"
+        rating="4 stars"
+      ></BookReview>
     </div>
   );
 };
--- src/components/BookReview.jsx
+++ src/components/BookReview.jsx
@@ -1,14 +1,14 @@
-const BookReview = () => {
+const BookReview = ({ title, author, review, rating }) => {
   return (
-    <div className="w-7/12 mx-auto p-4 flex flex-col bg-red-100 rounded-xl shadow-2xl">
-      <span>Title:</span>
-      <p>Atomic Habits</p>
-      <span>Author:</span>
-      <p>James Clear</p>
-      <span>Review:</span>
-      <p>An awesome self-help book</p>
-      <span>Rating:</span>
-      <p>4 stars</p>
+    <div className="w-5/6 md:w-1/3 mx-auto p-4 flex flex-col bg-red-100 rounded-xl shadow-2xl">
+      <span className="font-bold">Title:</span>
+      <p>{title}</p>
+      <span className="font-bold">Author:</span>
+      <p>{author}</p>
+      <span className="font-bold">Review:</span>
+      <p>{review}</p>
+      <span className="font-bold">Rating:</span>
+      <p>{rating}</p>
     </div>
   );
 };
